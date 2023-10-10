6980bc7c0f617cfc8a6dd83f6cf1399357dbe63f
Carsten Riedel <carstenriedel@outlook.com>
2023-10-10 23:22:27 +0200

    command and icon update

--- src/Coree.VisualStudio.DotnetToolbar/Command1.cs
+++ src/Coree.VisualStudio.DotnetToolbar/Command1.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using VSLangProj;
+using static System.Net.Mime.MediaTypeNames;
 using Task = System.Threading.Tasks.Task;
 
 namespace Coree.VisualStudio.DotnetToolbar
@@ -79,6 +86,16 @@ namespace Coree.VisualStudio.DotnetToolbar
             Instance = new Command1(package, commandService);
         }
 
+        private class ProjectInfo
+        {
+            public string FullProjectFileName { get; set; } = String.Empty;
+            public string FullPath { get; set; } = String.Empty;
+            
+            public string TargetFrameworks { get; set; } = String.Empty;
+            public List<string> TargetFrameworksList { get; set; } = new List<string>();
+            public string FriendlyTargetFramework { get; set; } = String.Empty;
+        }
+
         /// <summary>
         /// This function is the callback used to execute the command when the menu item is clicked.
         /// See the constructor to see how the menu item is associated with this function using
@@ -86,13 +103,54 @@ namespace Coree.VisualStudio.DotnetToolbar
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
-        private void Execute(object sender, EventArgs e)
+        private async void Execute(object sender, EventArgs e)
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte2 = await package.GetServiceAsync(typeof(DTE)).ConfigureAwait(false) as DTE2;
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            List<ProjectInfo> projectInfos =new List<ProjectInfo>();
 
-            EnvDTE.DTE dte = (EnvDTE.DTE)Microsoft.VisualStudio.Shell.ServiceProvider.GlobalProvider.GetService(typeof(EnvDTE.DTE));
-            string solutionDir = System.IO.Path.GetDirectoryName(dte.Solution.FullName);
-            dte.ExecuteCommand("Tools.Shell", $@"/o /c /dir:{solutionDir} dotnet.exe build");
+            Projects solProjects = dte2.Solution.Projects;
+            string solutionDir = System.IO.Path.GetDirectoryName(dte2.Solution.FullName);
+
+            foreach (Project item in solProjects)
+            {
+
+                var ProjectInfoItem = new ProjectInfo()
+                {
+                    FullProjectFileName = (string)item.Properties.Item("FullProjectFileName").Value,
+                    TargetFrameworks = (string)item.Properties.Item("TargetFrameworks").Value,
+                    FriendlyTargetFramework = (string)item.Properties.Item("FriendlyTargetFramework").Value,
+                    FullPath = (string)item.Properties.Item("FullPath").Value
+                };
+
+                if (ProjectInfoItem.TargetFrameworks.Split(';').Length > 0 )
+                {
+                    ProjectInfoItem.TargetFrameworksList.AddRange(ProjectInfoItem.TargetFrameworks.Split(';'));
+                }
+
+                if (ProjectInfoItem.TargetFrameworksList.Count == 0)
+                {
+                    ProjectInfoItem.TargetFrameworksList.Add(ProjectInfoItem.FriendlyTargetFramework);
+                }
+
+                projectInfos.Add(ProjectInfoItem);
+
+                foreach (Property items in item.Properties)
+                {  //FriendlyTargetFramework
+                    Debug.WriteLine($@"{items.Name},{items.Value}");
+                }
+             }
+
+            
+            SolutionConfiguration2 configuration = (SolutionConfiguration2)dte2.Solution.SolutionBuild.ActiveConfiguration;
+            Debug.WriteLine($@"ActiveConfiguration: {configuration.Name}{configuration.PlatformName}");
+
+            foreach (var item in projectInfos)
+            {
+                dte2.ExecuteCommand("Tools.Shell", $@"/o /c /dir:{item.FullPath} dotnet.exe build {item.FullProjectFileName} --configuration {configuration.Name}");
+            }
+
+            
         }
     }
 }
